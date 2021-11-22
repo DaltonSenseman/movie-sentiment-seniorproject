ed36111ad61a2244ed35e49de8e656a0aa716346
Carrie-West <54415496+Carrie-West@users.noreply.github.com>
2021-11-21 20:35:26 -0500

    Updating dataloader and datascrubber

--- Unprocessed Data Loader/data_loader.py
+++ Unprocessed Data Loader/data_loader.py
@@ -8,18 +8,44 @@ import sqlite3
 import data_scrubber as ds
 import sys
 import csv
+import json
 
-con = sqlite3.connect('review_database.db')
+con = sqlite3.connect('./review_database.db')
 
-table_creation = 'CREATE TABLE IF NOT EXISTS training (' \
+training_table_creation = 'CREATE TABLE IF NOT EXISTS training (' \
                  'review_id integer PRIMARY KEY,' \
                  'review text,' \
                  'sentiment integer)'
 
 cur = con.cursor()
 
-cur.execute(table_creation)
+cur.execute(training_table_creation)
 con.commit()
+
+cur.execute("DROP TABLE movies;")
+
+movie_table_creation = "CREATE TABLE IF NOT EXISTS movies (ref_num int PRIMARY KEY, name varchar, UNIQUE(name));"
+
+cur.execute(movie_table_creation)
+con.commit()
+
+
+processing_table_creation = "CREATE TABLE IF NOT EXISTS review (review_id varchar PRIMARY KEY," \
+                            "reviewer varchar," \
+                            "movie_id int," \
+                            "rating int," \
+                            "review_summary varchar," \
+                            "review_date varchar," \
+                            "spoiler_tag boolean, " \
+                            "review_detail varchar," \
+                            "helpful int," \
+                            "not_helpful int," \
+                            "FOREIGN KEY(movie_id) REFERENCES movie(ref_num));"
+
+cur.execute(processing_table_creation)
+con.commit()
+
+
 
 target = sys.argv[1]
 file = sys.argv[2]
@@ -33,7 +59,8 @@ statements = []
 if target.lower() == "training":
     print("training")
     with open(file, newline='', encoding="utf8") as csvfile:
-        training_batch_statement = 'INSERT INTO training VALUES'
+        training_batch_statement = 'INSERT OR IGNORE INTO ' \
+                                   'training VALUES'
         reader = csv.reader(csvfile, delimiter='\"', quotechar='|')
         next(reader, None)
         counter = 0
@@ -46,16 +73,20 @@ if target.lower() == "training":
                 review = ds.clean_string(''.join(row[1:-1]))
 
             try:
-                review_addition = ds.package_training_review(counter, review, sentiments[sentiment])
-                training_batch_statement = training_batch_statement + review_addition + ','
+                review_addition = ds.package_training_review(
+                    counter, review, sentiments[sentiment])
+                training_batch_statement = training_batch_statement +\
+                                           review_addition + ','
 
             except KeyError:
                 fixed_review = row[-1].rsplit(',', 1)
 
                 review = ds.clean_string(fixed_review[0])
 
-                review_addition = ds.package_training_review(counter, review, sentiments[fixed_review[1]])
-                training_batch_statement = training_batch_statement + review_addition + ','
+                review_addition = ds.package_training_review(
+                    counter, review, sentiments[fixed_review[1]])
+                training_batch_statement = training_batch_statement + \
+                                           review_addition + ','
 
             if (counter % 1000 == 0):
 
@@ -67,14 +98,39 @@ if target.lower() == "training":
                 except sqlite3.OperationalError as e:
                     print(e)
 
-                training_batch_statement = 'INSERT INTO training VALUES '
+                training_batch_statement = 'INSERT INTO training ' \
+                                           'VALUES '
 
 
 elif target.lower() == "processing":
     print("processing")
+    with open(file, newline='', encoding="utf8") as jsonfile:
+        data = json.load(jsonfile)
+        movie_count_statement = ("SELECT COUNT(*) from movies")
+        cur.execute(movie_count_statement)
+        count = cur.fetchone()[0]
+        movie_statement = f"INSERT INTO movies VALUES"
+        for review in data:
+            try:
+                cur.execute(ds.package_movie(count, review['movie']))
+                print(ds.package_movie(count, review['movie']))
+            except sqlite3.IntegrityError:
+                try:
+                    cur.execute(f"SELECT ref_num from movies WHERE name = {review['movie']};")
+                    print(ds.package_processing_review(review))
+                except sqlite3.OperationalError as e:
+                    print(e)
+                    print(ds.package_movie(count, review['movie']))
+                    break
 
-select = 'SELECT * from training;'
+            count = count + 1
 
-cur.execute(select)
+
+
+elif target.lower() == "display_training":
+    select = 'SELECT * from training LIMIT 1;'
+    cur.execute(select)
+    rows = cur.fetchall()
+    print(rows)
 
 con.close()
--- Unprocessed Data Loader/data_scrubber.py
+++ Unprocessed Data Loader/data_scrubber.py
@@ -14,7 +14,36 @@ def clean_string(string):
     #cleaned_string = cleaned_string.replace(')', ']')
     return cleaned_string
 
-def package_training_review(counter, review, sentiment):
-    response = '(' + str(counter) + ',\'' + review + '\',' + str(sentiment) + ')'
 
+def package_training_review(counter, review, sentiment):
+    response = '(' + str(counter) + ',\'' + review + '\',' + \
+               str(sentiment) + ')'
+
+    return response
+
+
+# "CREATE TABLE IF NOT EXISTS review (review_id varchar PRIMARY KEY," \
+#                           "reviewer varchar," \
+#                          "movie_id int," \
+#                         "rating int," \
+#                        "review_summary varchar," \
+#                       "review_date varchar," \
+#                      "spoiler_tag boolean, " \
+#                     "review_detail varchar," \
+#                    "helpful int," \
+#                   "not_helpful int," \
+#                  "FOREIGN KEY(movie_id) REFERENCES movie(ref_num));"
+
+def package_processing_review(review):
+    response = f"({review['review_id']}, {review['reviewer']}, " \
+               f"{review['rating']}," \
+               f"{clean_string(review['review_summary'])}, {review['review_date']}," \
+               f" {bool(review['spoiler_tag'])}," \
+               f"{clean_string(review['review_detail'])},{review['helpful'][0]}," \
+               f"{review['helpful'][1]})"
+    return response
+
+def package_movie(count, title):
+    movie_title = '\'' + clean_string(title) + '\''
+    response = f"INSERT INTO movies VALUES({count}, {movie_title});"
     return response
