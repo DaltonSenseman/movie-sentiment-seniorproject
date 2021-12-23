b86a99032533249f3b3418ba9de78c230a209c16
Dalton Senseman <43663619+DaltonSenseman@users.noreply.github.com>
2021-12-23 13:11:12 -0500

    Formed the start of the method to generate sentiment and build the ML algorithm in.

--- NaiveBayes Machine Learning/NaiveBayes.py
+++ NaiveBayes Machine Learning/NaiveBayes.py
@@ -53,6 +53,32 @@ def create_dictionary(database_connection, value):
     return dictionary
 
 
+def sentiment_generator(review_data, test_data_pos, test_data_neg):
+    """
+    Takes in a single review and checks it against the positive and negative data lists to generate the sentiment
+    :param review_data: the review dictionary of the review we are creating the sentiment for
+    :param test_data_pos: the positive dictionary of training data to check against for the ML
+    :param test_data_neg: the negative dictionary of training data to check against for the ML
+    :return: will return the score of sentiment for that particular review
+             (0 for negative 1 for positive) -1 is a placeholder for now
+    """
+    for key in review_data.keys():
+        print("\"" + key + "\"" + " value -> " + str(review_data.get(key)))  # get value of that key in the dict
+
+        if key in test_data_pos.keys():
+            print("pos HIT")
+            print(test_data_pos.get(key))  # get value of that key in the dict
+        if key in test_data_neg.keys():
+            print("neg HIT")
+            print(test_data_neg.get(key))  # get value of that key in the dict
+        else:
+            print("no Key need a black box (artificial value to not have * or / by Zero errors)")
+
+    sentiment_score = -1
+
+    return sentiment_score
+
+
 def main():
     sentiment = SQLManager()
 
@@ -60,9 +86,9 @@ def main():
     pos_dict = create_dictionary(sentiment, 1)
 
     # total number of words in the entire dictionary for use in making fractional probability
-    pos_dict_TOTAL = sum(pos_dict.values())  # total number 5702647
-    neg_dict_TOTAL = sum(neg_dict.values())  # total number 5609804
-    testdata_TOTAL = (pos_dict_TOTAL + neg_dict_TOTAL)  # total 113124451
+    pos_dict_TOTAL = sum(pos_dict.values())  # total number of words 5,702,647
+    neg_dict_TOTAL = sum(neg_dict.values())  # total  number of words 5,609,804
+    testdata_TOTAL = (pos_dict_TOTAL + neg_dict_TOTAL)  # total data points(words) 113,124,451
 
     pos_prior_probability = (pos_dict_TOTAL / testdata_TOTAL)  # .5041
     neg_prior_probability = (neg_dict_TOTAL / testdata_TOTAL)  # .4958
@@ -74,19 +100,14 @@ def main():
 
     # testing searching in the positive and negative lists for matching keys
 
-    for key in review_test.keys():
-        if key in pos_dict.keys():
-            print("pos Yes")
-        if key in neg_dict.keys():
-            print("neg Yes")
-        else:
-            print("no Key need a black box (artificial value to not have * or / by Zero errors)")
+    # method below will return the score of the review to then be pushed into the DB
 
-    print(review_test)
+    print(sentiment_generator(review_test, pos_dict, neg_dict))
 
     # after compiling matching keys we can then get probabilities and do the algorithm :D
     # setting the value into a after-ML Table in the DB and iterate though the entire pre-ML data set
     # then we can tinker with accuracy -Dalton S.
+
 
 """             START OF PROGRAM                      """
 
